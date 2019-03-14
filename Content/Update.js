2dd20e665326fa470d353dd659b28647db03dd51
kakasg01 <48540241+kakasg01@users.noreply.github.com>
2019-03-14 13:45:51 +0700

    Figurine

---
+++ Content/Update.js
@@ -0,0 +1,16 @@
+function check()
+{
+	var Title = f2.tt.value;
+	var Price = f2.p.value;
+	var Character = f2.c.value;
+	var Series = f2.s.value;
+	var Manafacturer = f2.m.value;
+	var Size = f2.si.value;
+	
+	document.write("Title: " + Title);
+	document.writeln("Price: " + Price );
+	document.writeln("Character: " + Character);
+	document.writeln("Series: " + Series);
+	document.writeln("Manafacturer: " + Manafacturer);
+	document.writeln("Size: " + Size);
+}
